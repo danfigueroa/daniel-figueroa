abbf6cac525f4205b91406d818a405e241ae10b7
dan figueroa <danielmfigueroa@gmail.com>
2025-02-11 22:53:14 -0300

    feat: add suport to custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/daniel-figueroa', // Deve corresponder ao nome do seu repositório
+    base: '/', // Para domínio personalizado, use apenas '/'
 })
